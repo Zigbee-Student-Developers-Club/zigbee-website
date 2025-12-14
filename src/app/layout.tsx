d5422deabb4d43690a918bc9b1f8fd3d7c51301e
Manas Kumar Pradhan <manas.pradhan@pixelcompute.com>
2025-12-14 06:00:10 +0530

    Initialized toastr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import { ToastContainer } from "react-toastify";
 
 import ThemeProvider from "@/providers/Theme";
 
@@ -26,6 +27,7 @@ const RootLayout = ({
         >
           {children}
         </ThemeProvider>
+        <ToastContainer />
       </body>
     </html>
   );
