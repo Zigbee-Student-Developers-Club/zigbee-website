b3d0f32d002900e55b1e0aa097a974b031a7cd70
Manas Kumar Pradhan <manas.pradhan@pixelcompute.com>
2025-12-14 21:48:28 +0530

    Updated footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ const RootLayout = ({
       <body
         className={classNames(
           nunito.className,
-          "antialiased overflow-x-hidden flex flex-col w-full"
+          "antialiased overflow-x-hidden flex flex-col w-full min-h-screen"
         )}
       >
         <ThemeProvider
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,13 @@
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+
 import routes from "@/routes";
 
-export const navItems = [
+export interface NavItem {
+  name: string;
+  link: string;
+}
+
+export const navItems: NavItem[] = [
   {
     name: "Home",
     link: routes.home,
@@ -21,4 +28,37 @@ export const navItems = [
     name: "Alumni",
     link: routes.alumni,
   },
+];
+
+export const footerItemLinks = [
+  {
+    label: "About & Team",
+    value: [
+      { label: "About Us", href: "/about" },
+      { label: "Our Team", href: "/team" },
+      { label: "Mission", href: "/mission" },
+      { label: "Join Us", href: "/join" },
+    ],
+  },
+  {
+    label: "Popular Links",
+    value: [
+      { label: "Events", href: "/events" },
+      { label: "Workshops", href: "/workshops" },
+      { label: "Projects", href: "/projects" },
+      { label: "Resources", href: "/resources" },
+      { label: "Alumni", href: "/alumni" },
+    ],
+  },
+];
+
+export const socialLinks = [
+  { label: "LinkedIn", href: "https://linkedin.com", icon: FaLinkedin },
+  { label: "Instagram", href: "https://instagram.com", icon: FaInstagram },
+  { label: "GitHub", href: "https://github.com", icon: FaGithub },
+];
+
+export const contactInfo = [
+  { label: "Email", value: "zigbee@outr.ac.in" },
+  { label: "Location", value: "Student Activity Center" },
 ];
