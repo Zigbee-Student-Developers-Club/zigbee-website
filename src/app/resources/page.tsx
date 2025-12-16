62e95a5729332bf60ce399f2d928d2285ee0457c
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-12-16 10:25:45 +0530

    used barrel exports for PageHeader component

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import PageHeader from "@/components/commons/PageHeader";
+import { PageHeader } from "@/components/commons";
 
 const Resources = () => {
   return (
--- src/components/commons/index.ts
+++ src/components/commons/index.ts
@@ -1,4 +1,5 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import PageHeader from "./PageHeader";
 
-export { Header, Footer };
+export { Header, Footer, PageHeader };
