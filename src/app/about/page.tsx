23b9360d14f5d5e7f9619246d7bc2f9f1d161997
sankarshan <ssankarshan636@gmail.com>
2025-12-17 18:19:43 +0530

    Added about-us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import OurHistory from "@/components/OurHistory/OurHistory";
+import MeetTheTeam from "@/components/Team/MeetTheTeam";
+
 const About = () => {
-  return <div>About page</div>;
+  return (
+    <div className="space-y-6">
+      <OurHistory />
+      <MeetTheTeam />
+    </div>
+  );
 };
 
 export default About;
