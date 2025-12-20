4d1e61ade33d93e74b7c14ec57efbc779ec375b0
sankar636 <ssankarshan636@gmail.com>
2025-12-20 11:46:55 +0530

    Implemented about us page (#41)
    * Added about image
    
    * Added OurHistory component
    
    * Added TeaCard component
    
    * Added MeeTheTeam Component
    
    * Added about-us page
    
    * Implemented about us page
    
    ---------
    
    Co-authored-by: Manas Kumar Pradhan <pkmanas22@gmail.com>
    Co-authored-by: Manas Kumar Pradhan <manas.pradhan@pixelcompute.com>

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import { Team, About as AboutComponent } from "@/components/AboutTeam";
+import { PageHeader } from "@/components/commons";
+
 const About = () => {
-  return <div>About page</div>;
+  return (
+    <>
+      <PageHeader
+        // backgroundImage="/images/events-bg.jpg"
+        description="The story behind the club and the people who make it happen."
+        title="About Zigbee"
+      />
+      <div className="px-10">
+        <AboutComponent />
+        <Team />
+      </div>
+    </>
+  );
 };
 
 export default About;
